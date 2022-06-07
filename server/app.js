2e80dfc938a63ca4cf93370f937f2ea80b696835
raul crespo <raulcrespo@outlook.com.br>
2022-06-07 17:05:37 -0300

    add cors

--- server/app.js
+++ server/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 CONNECTION_URL = 'mongodb+srv://admin:adm123@cluster0.zpefa.mongodb.net/?retryWrites=true&w=majority'
+
 mongoose.connect(CONNECTION_URL);
 mongoose.connection.once('open', () => {
     console.log('Connection to database has been established successfully');
