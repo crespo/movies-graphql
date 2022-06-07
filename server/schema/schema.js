83b3c687214b4a6d0641633aa1a5a2d87d50e86b
raul crespo <raulcrespo@outlook.com.br>
2022-06-07 10:23:47 -0300

    add add mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Movie = require('../models/movie');
+const Director = require('../models/director');
 
 const { 
     GraphQLObjectType, 
@@ -9,23 +11,6 @@ const {
     GraphQLInt,
     GraphQLList
 } = graphql;
-
-const movies = [
-    { name: 'Joker', genre: 'Drama', id: '1', directorId: '1'},
-    { name: 'Moonrise Kingdom', genre: 'Romance', id: '2', directorId: '2' },
-    { name: 'La La Land', genre: 'Musical', id: '3', directorId: '3' },
-    { name: 'Interestellar', genre: 'Sci-fi', id: '4', directorId: '4' },
-    { name: 'The Grand Budapest Hotel', genre: 'Comedy', id: '5', directorId: '2' },
-    { name: 'Whiplash', genre: 'Drama', id: '6', directorId: '3' },
-    { name: 'First Man', genre: 'Biography', id: '7', directorId: '3' }
-]
-
-const directors = [
-    { name: 'Todd Philips', age: 60, id: '1' },
-    { name: 'Wes Anderson', age: 52, id: '2' },
-    { name: 'Damien Chazelle', age: 58, id: '3' },
-    { name: 'Christopher Nolan', age: 51, id: '4' }
-]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -37,7 +22,7 @@ const MovieType = new GraphQLObjectType({
             type: DirectorType,
             resolve(parent, args) {
                 console.log(parent)
-                return _.find(directors, { id: parent.directorId })
+                // return _.find(directors, { id: parent.directorId })
             }
         }
     })
@@ -52,7 +37,7 @@ const DirectorType = new GraphQLObjectType({
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return _.filter(movies, { directorId: parent.id })
+                // return _.filter(movies, { directorId: parent.id })
             }
         }
     })
@@ -67,31 +52,67 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 // get data from database
                 console.log(typeof args.id)
-                return _.find(movies, { id: args.id });
+                // return _.find(movies, { id: args.id });
             }
         },
         director: {
             type: DirectorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return _.find(directors, { id: args.id });
+                // return _.find(directors, { id: args.id });
             }
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies;
+                // return movies;
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors;
+                // return directors;
             }
         }
     })
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addDirector: {
+            type: DirectorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let director = new Director({
+                    name: args.name,
+                    age: args.age
+                });
+                return director.save();
+            }
+        },
+        addMovie: {
+            type: MovieType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString},
+                directorId: { type: GraphQLID }
+            }, resolve(parent, args) {
+                let movie = new Movie({
+                    name: args.name,
+                    genre: args.genre,
+                    directorId: args.directorId
+                });
+                return movie.save();
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
     query: RootQuery,
+    mutation: Mutation
 })
