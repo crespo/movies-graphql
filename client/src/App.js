b9560d5840d909d4a3a120bbc094f9870a962f69
raul crespo <raulcrespo@outlook.com.br>
2022-06-07 15:18:24 -0300

    add react-based frontend files

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import MovieList from "./components/MovieList";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Watch List</h1>
+        <MovieList/>
+      </div>
+    </ApolloProvider>
+  );
+};
+
+export default App;
---
+++ client/src/components/MovieList.js
@@ -0,0 +1,12 @@
+function MovieList() {
+    return (
+      <div className="App">
+        <ul>
+            <li>Movie List</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default MovieList;
+  
