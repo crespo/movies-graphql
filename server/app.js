de618fe160d218abf18f32f7faa04c249fc706d6
raul crespo <raulcrespo@outlook.com.br>
2022-06-07 09:18:00 -0300

    add mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+CONNECTION_URL = 'mongodb+srv://admin:adm123@cluster0.zpefa.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_URL);
+mongoose.connection.once('open', () => {
+    console.log('Connection to database has been established successfully');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -11,4 +18,4 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(5000, () => {
     console.log('Server is up and running on port 5000');
-})
+});
